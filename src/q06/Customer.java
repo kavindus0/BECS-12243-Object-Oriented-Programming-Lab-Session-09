f413552544a6be4a904af56b8c09962275cccdb2
kavindus0 <kavindusachinthe@outlook.com>
2024-12-26 19:00:46 +0530

    comment deleted

--- src/q06/Customer.java
+++ src/q06/Customer.java
@@ -1,18 +1,6 @@
 package q06;
 
 
-
-//            Customer
-
-//+bookRoom(room:Room):void
-//+ displayBookingDetails():void
-//    If is Available true in Room class
-//    call bookRoom() and assign bookedRoom as room print "[Customer Name] has booked room [Room Number]"
-//            else print "Room [Room Number] is not available"
-//    Display Customer ID, Customer Name, and
-//if booked rooms
-//    display room details
-//else print "No room is Booked"
 public class Customer {
     String customerID;
     String name;
@@ -24,6 +12,8 @@ public class Customer {
         }
         this.bookedRoom = bookedRoom;
     }
+
+
 
     public Customer(String customerID, String name) {
         this.customerID = customerID;
